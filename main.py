a8e25b54fdc1558ac8b2224294abd4894e7a9494
Hunter Boyd <hunterjsb@gmail.com>
2021-11-04 15:15:58 -0400

    * $leaderboard now tells you to use $scoreboard * added import requests * began to add support for jsonbin (unimplemented)

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import discord
 from discord.ext import commands
 import random
 import string
+import requests
 
 with open('secrets.json', 'r') as secrets_file:
-    TOKEN = json.load(secrets_file)['DISCORD_TOKEN']
+    secrets = json.load(secrets_file)
+    TOKEN = secrets['DISCORD_TOKEN']
+    JSONBIN_KEY = secrets['JSONBIN_KEY']
 bot = commands.Bot(command_prefix='$')
 
 
@@ -56,6 +59,32 @@ PFP_URLS = {
     'defect': 'https://cdn.pastemagazine.com/www/articles/slay%20the%20spire%20defect%20main.jpg',
     'watcher': 'https://www.dexerto.com/wp-content/uploads/2019/11/akali-cosplay.jpg'
 }
+
+# Collection ID to add new json files to the correct jsonbin collection
+TIMESLUG_COLLECTION = {"record": "6184060aaa02be1d4463f075",
+                       "metadata": {"createdAt": "2021-11-04T16:10:50.995Z",
+                                    "name": "TimeSlug"}}
+RECORD = TIMESLUG_COLLECTION['record']
+
+
+def update_keys():
+    """post new API keys to JSONBIN, update existing keys on JSONBIN"""
+    pass
+
+
+def jsonbin_scores_commit():
+    """commit the entire local scores files to jsonbin"""
+    headers = {
+        'Content-Type':
+        'application/json',
+        'X-Master-Key': JSONBIN_KEY
+    }
+
+    with open('scores.json', 'r') as f:
+        data = json.load(f)
+
+    req = requests.post('https://api.jsonbin.io/v3/b', json=data, headers=headers)
+    print(req.text)
 
 
 # returns today's date as a string in the format used as the top-level keys in scores.json
@@ -117,39 +146,47 @@ async def score(ctx, player_score, *args):
 
 
 @bot.command()
-async def leaderboard(ctx, day='today'):
+async def scoreboard(ctx, day='today'):
     if day == 'today':
         day = update_date()
     with open('scores.json', 'r') as f:
         try:
-            scoreboard = json.load(f)[day]
+            daily_challenge = json.load(f)[day]
         except KeyError:
             await ctx.send('No scoreboard for today!')
             return
 
     # SORTING
     # sort the player id's according to score
-    scores = sorted(scoreboard['scores'], key=scoreboard['scores'].get, reverse=True)
+    scores = sorted(daily_challenge['scores'], key=daily_challenge['scores'].get, reverse=True)
     winner = await bot.fetch_user(int(scores[0]))
 
     # EMBED CONSTRUCTION
     # create and embed leaderboard
     pasta = discord.Embed(
         title=f'Daily Challenge, {day}',
-        description=f'*{" ".join(scoreboard["modifiers"])} __{scoreboard["character"]}__*',
+        description=f'*{" ".join(daily_challenge["modifiers"])} __{daily_challenge["character"]}__*',
         color=discord.Color.blurple()
     )
     # add the users to embed
     for i, player_id in enumerate(scores):
         user = await bot.fetch_user(int(player_id))
-        pasta.add_field(name=f'{i+1}. {user.name}:', value=f'`{scoreboard["scores"][player_id]}`', inline=False)
+        pasta.add_field(name=f'{i+1}. {user.name}:', value=f'`{daily_challenge["scores"][player_id]}`', inline=False)
     # add the character image
-    if scoreboard["character"] in PFP_URLS:
-        pasta.set_image(url=PFP_URLS[scoreboard['character']])
+    if daily_challenge["character"] in PFP_URLS:
+        pasta.set_image(url=PFP_URLS[daily_challenge['character']])
     # add winner thumbnail
     pasta.set_thumbnail(url=winner.avatar_url)
     # send the message
     await ctx.send(embed=pasta)
+
+
+@bot.command()
+async def leaderboard(ctx):
+    await ctx.send('try $scoreboard')
+
+    with open('scores.json', 'f') as f:
+        scores = json.load(f)
 
 
 @bot.command()
